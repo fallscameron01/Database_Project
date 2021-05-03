bb76afc8636263280cabdd5ce4be75378017e9ff
Henri Evjen <henrievjen@gmail.com>
2021-05-02 21:33:14 -0500

    added filters to Library and Wishlist pages

--- front-end/src/components/library/index.js
+++ front-end/src/components/library/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import addNew from "./images/add-new.PNG";
 
 // This is hardcoded data for now
@@ -7,39 +7,77 @@ const data = [
   {
     title: "Halo",
     box_art: "https://www.mobygames.com/images/covers/l/9494-halo-combat-evolved-xbox-front-cover.jpg",
-    description: "Shooter Video Game"
+    description: "Shooter Video Game",
+    type: "Video Game"
   },
   {
     title: "Call of Duty",
     box_art: "https://upload.wikimedia.org/wikipedia/en/f/fd/Call_of_Duty_-_Infinite_Warfare_%28promo_image%29.jpg",
-    description: "Shooter Video Game"
+    description: "Shooter Video Game",
+    type: "Video Game"
   },
   {
     title: "Rocket League",
     box_art: "https://images-na.ssl-images-amazon.com/images/I/71mzNfoyPUL._SL1000_.jpg",
-    description: "Car Video Game"
+    description: "Car Video Game",
+    type: "Video Game"
   },
   {
     title: "Harry Potter",
     box_art: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/3406/34068479_so.jpg",
-    description: "Wizard dude does some cool stuff"
+    description: "Wizard dude does some cool stuff",
+    type: "Movie"
   }
 ];
 
 function Library() {
+  const [type, setType] = useState("All");
+  const [sort, setSort] = useState("alphabetical");
+
+  const handleType = () => {
+    const value = document.getElementById("types");
+    setType(value.value);
+  };
+
+  const handleSort = () => {
+    const value = document.getElementById("sort");
+
+    if(value.value === "alphabetical") {
+      setSort("alphabetical");
+    }
+    else {
+      setSort("reverse");
+    }
+  };  
+
   return (
     <div style={{padding: 50}}>
+      <div style={{display: "block"}}>
+        <select onChange={handleType} id="types" style={{marginRight: 10}}>
+          <option value="All" selected="selected">All</option>
+          <option value="Video Game">Video Game</option>
+          <option value="Movie">Movie</option>
+          <option value="Music">Music</option>
+          <option value="Book">Book</option>
+        </select>
+
+        <select onChange={handleSort} id="sort">
+          <option value="alphabetical" selected="selected">Alphabetical (Title)</option>
+          <option value="reverse">Reverse Alphabetical (Title)</option>
+        </select>
+      </div>
+
       <div style={{display: "inline-block"}}>
-        <img src={addNew} width="200px" height="270px" style={{margin: 15}} />
+        <img src={addNew} width="auto" height="270px" style={{margin: 15}} />
         <h5 className="text-center">Add New</h5>
       </div>
       
-        {data.map((el) => {
-            return (
+        {(sort === "alphabetical" ? data.sort((a, b) => (a.title > b.title) ? 1 : -1) : data.sort((a, b) => (a.title < b.title) ? 1 : -1)).map((el) => {
+            return (el.type === type || type === "All" ?
               <div style={{display: "inline-block"}}>
-                <img src={el.box_art} width="200px" height="270px" style={{margin: 15}} />
+                <img src={el.box_art} width="auto" height="270px" style={{margin: 15}} />
                 <h5 className="text-center">{el.title}</h5>
-              </div>
+              </div> : <></>
             )
         })}
     </div>
--- front-end/src/components/wishlist/index.js
+++ front-end/src/components/wishlist/index.js
@@ -1,36 +1,84 @@
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import addNew from "./images/add-new.PNG";
 
 // This is hardcoded data for now
 // We will get this data from the database using an API call
 const data = [
   {
-    title: "Call of Duty",
-    box_art: "https://upload.wikimedia.org/wikipedia/en/f/fd/Call_of_Duty_-_Infinite_Warfare_%28promo_image%29.jpg",
-    description: "Shooter Video Game"
+    title: "Fall Guys",
+    box_art: "https://upload.wikimedia.org/wikipedia/en/5/5e/Fall_Guys_cover.jpg",
+    description: "You fall",
+    type: "Video Game"
   },
   {
-    title: "Harry Potter",
-    box_art: "https://pisces.bbystatic.com/image2/BestBuy_US/images/products/3406/34068479_so.jpg",
-    description: "Wizard dude does some cool stuff"
+    title: "Goodnight Moon",
+    box_art: "https://prodimage.images-bn.com/pimages/9780062573094_p0_v1_s550x406.jpg",
+    description: "The moon goes to bed",
+    type: "Book"
   },
   {
-    title: "Rocket League",
-    box_art: "https://images-na.ssl-images-amazon.com/images/I/71mzNfoyPUL._SL1000_.jpg",
-    description: "Car Video Game"
+    title: "Tenet",
+    box_art: "https://m.media-amazon.com/images/M/MV5BYzg0NGM2NjAtNmIxOC00MDJmLTg5ZmYtYzM0MTE4NWE2NzlhXkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg",
+    description: "A movie that will make no sense",
+    type: "Movie"
   },
   {
-    title: "Halo",
-    box_art: "https://www.mobygames.com/images/covers/l/9494-halo-combat-evolved-xbox-front-cover.jpg",
-    description: "Shooter Video Game"
+    title: "Dark Side of the Moon",
+    box_art: "https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png",
+    description: "Good stuff",
+    type: "Music"
   }
 ];
 
 function Wishlist() {
+  const [type, setType] = useState("All");
+  const [sort, setSort] = useState("alphabetical");
+
+  const handleType = () => {
+    const value = document.getElementById("types");
+    setType(value.value);
+  };
+
+  const handleSort = () => {
+    const value = document.getElementById("sort");
+
+    if(value.value === "alphabetical") {
+      setSort("alphabetical");
+    }
+    else {
+      setSort("reverse");
+    }
+  };
+
   return (
     <div style={{padding: 50}}>
-        {data.map((el) => {
-          return (<img src={el.box_art} width="200px" height="300px" style={{margin: 15}} />)
+      <div style={{display: "block"}}>
+        <select onChange={handleType} id="types" style={{marginRight: 10}}>
+          <option value="All" selected="selected">All</option>
+          <option value="Video Game">Video Game</option>
+          <option value="Movie">Movie</option>
+          <option value="Music">Music</option>
+          <option value="Book">Book</option>
+        </select>
+
+        <select onChange={handleSort} id="sort">
+          <option value="alphabetical" selected="selected">Alphabetical (Title)</option>
+          <option value="reverse">Reverse Alphabetical (Title)</option>
+        </select>
+      </div>
+
+      <div style={{display: "inline-block"}}>
+        <img src={addNew} width="auto" height="270px" style={{margin: 15}} />
+        <h5 className="text-center">Add New</h5>
+      </div>
+      
+        {(sort === "alphabetical" ? data.sort((a, b) => (a.title > b.title) ? 1 : -1) : data.sort((a, b) => (a.title < b.title) ? 1 : -1)).map((el) => {
+            return (el.type === type || type === "All" ?
+              <div style={{display: "inline-block"}} title={el.description}>
+                <img src={el.box_art} width="auto" height="270px" style={{margin: 15}} />
+                <h5 className="text-center">{el.title}</h5>
+              </div> : <></>
+            )
         })}
     </div>
   );
