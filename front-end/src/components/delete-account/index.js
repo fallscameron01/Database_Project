44128b6a945399617cafaf1227968094d8f57d12
Cameron Falls <camfalls0201@gmail.com>
2021-05-04 17:21:31 -0500

    Change delete redirect to welcome

--- front-end/src/components/delete-account/index.js
+++ front-end/src/components/delete-account/index.js
@@ -7,7 +7,7 @@ function DeleteAccount() {
         <p class="delete_message">Are you sure you wish to delete your account? This cannot be undone!</p>
 
         <div class="delete_confirm">
-          <a href="login">Yes</a>
+          <a href="welcome">Yes</a>
         </div>
         <div/>
         <div class="delete_cancel">
