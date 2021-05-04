531e997e6a362f4682b2b763f39da8b9e4ecab79
Henri Evjen <henrievjen@gmail.com>
2021-05-04 17:21:03 -0500

    sign up creates user

--- front-end/src/components/signup/index.js
+++ front-end/src/components/signup/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import UserDataService from "../../services/user.service";
 import "./index.css";
 
 function Signup() {
   const handleSubmit = () => {
-    // this is where I would put code to sign up.
-    // IF I HAD ANY!
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value; 
+    
+    UserDataService.create({username, password});
   };
 
   return (
@@ -12,15 +15,15 @@ function Signup() {
       <div class="signup_form">
         <form onSubmit={handleSubmit}>
           <label>Username:</label><div/>
-          <input type="text" name="username"/><div/>
+          <input type="text" name="username" id="username" /><div/>
 
           <label>Password:</label><div/>
-          <input type="text" name="password"/><div/>
+          <input type="text" name="password" id="password" /><div/>
 
           <label>Repeat Password:</label><div/>
           <input type="text" name="repeat_password"/><div/>
 
-          <input type="submit" value="Sign Up"/>
+          <input type="submit" value="Sign Up" />
         </form>
       </div>
     </div>
--- front-end/src/services/user.service.js
+++ front-end/src/services/user.service.js
@@ -1,8 +1,23 @@
-import http from "../http-common";
+var axios = require('axios');
 
 class UserDataService {
   create(data) {
-    return http.post("/user", data);
+    var config = {
+      method: 'post',
+      url: 'http://localhost:8080/api/user',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
 }
 
